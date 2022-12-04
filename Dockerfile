3a22f37855e11874c6271c85ef664d002e962a9a
nidhishnanavati <nidhishnanavati@gmail.com>
2022-12-04 18:53:19 -0500

    Updated restcall.services.ts and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,4 @@ RUN npm install
 RUN npm run build --prod
 FROM nginx:alpine
 COPY --from=node /app/dist/angular /usr/share/nginx/html
+EXPOSE 80
--- src/app/restcall.service.ts
+++ src/app/restcall.service.ts
@@ -10,10 +10,10 @@ export class RestcallService {
   constructor(private http:HttpClient) { }
 
   public submit(model: Model){
-    return this.http.post("http://ec2-52-23-185-171.compute-1.amazonaws.com:31707/submitSurvey", model);
+    return this.http.post("http://34.74.81.186/submitSurvey", model);
   }  
 
   public fetch(){
-    return this.http.get("http://ec2-52-23-185-171.compute-1.amazonaws.com:31707/getAllSurveys");
+    return this.http.get("http://34.74.81.186/getAllSurveys");
   }
 }
